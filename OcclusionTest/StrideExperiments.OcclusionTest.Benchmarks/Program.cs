8c0f6ef4e9b6c9b202673f378e924ba640d1fa2b
John Ludlow <john.ludlow@halfords.co.uk>
2025-05-01 23:03:36 +0100

    #5 Add occlusion test benchmark

---
+++ OcclusionTest/StrideExperiments.OcclusionTest.Benchmarks/Program.cs
@@ -0,0 +1,23 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+using StrideExperiments.OcclusionTest.UnitTests;
+
+[SimpleJob(RuntimeMoniker.Net80)]
+[PlainExporter]
+public class OcclusionTest
+{
+    [Benchmark]
+    public void Benchmark()
+    {
+        var benchmark = new ObjectInViewTest();
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var summary = BenchmarkRunner.Run<OcclusionTest>();
+    }
+}
